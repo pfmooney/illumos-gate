edb84658c51db859cdfe58a660c3b52d9c8616de
Patrick Mooney <pmooney@pfmooney.com>
2025-07-24 23:36:19 +0000

    XXX: fix polarity for interrupt forwarding

--- usr/src/uts/common/io/cxgbe/t4nex/t4_sge.c
+++ usr/src/uts/common/io/cxgbe/t4nex/t4_sge.c
@@ -1026,7 +1026,7 @@ t4_alloc_iq(struct port_info *pi, const struct t4_iq_params *tip,
 		.type_to_iqandstindex = BE_32(
 		    /* Special handling for firmware event queue */
 		    (is_fwq ? F_FW_IQ_CMD_IQASYNCH : 0) |
-		    (intr_fwd ? 0 : F_FW_IQ_CMD_IQANDST) |
+		    (intr_fwd ? F_FW_IQ_CMD_IQANDST : 0) |
 		    V_FW_IQ_CMD_IQANDSTINDEX(intr_idx) |
 		    V_FW_IQ_CMD_TYPE(FW_IQ_TYPE_FL_INT_CAP) |
 		    V_FW_IQ_CMD_VIID(pi->viid) |
